a42aa753a944960b7ced324122c2e9f0a7f84104
rindra josia <rrindrajosia@gmail.com>
2021-08-11 14:32:20 +0300

    Create a basic server

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,19 @@
 const express = require('express');
+const products = require('./data/products');
 
 const app = express();
+
+app.get('/', (request, response) => {
+  response.send('API is running...')
+})
+
+app.get('/api/products', (request, response) => {
+  response.json(products)
+})
+
+app.get('/api/products/:id', (request, response) => {
+  const product = products.find(p => p._id === request.params.id)
+  response.json(product)
+})
 
 app.listen(5000, console.log('Server running on port 5000'));
