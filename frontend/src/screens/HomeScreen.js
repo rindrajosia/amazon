434c3938f593567a8ad2142878f6976fa2e11111
rindra josia <rrindrajosia@gmail.com>
2021-08-15 09:11:50 +0300

    Fetch products from server

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
-import products from '../products';
+import axios from 'axios';
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await axios.get('/api/products');
+      setProducts(response.data);
+    }
+    fetchProducts
+  }, [])
   return (
     <>
       <h1>Latests Products</h1>
